d852b334289ce42f5068639ab648014ec9fd3e8d
lambeb <141648830+lambeb@users.noreply.github.com>
2025-05-20 15:23:32 +0100

    BShift seems to be blank... trying SelectFlight

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -47,7 +47,7 @@ def step_impl(context):
                 Baseport=fields.get("qShift.Baseport", None),
                 Linecode=fields.get("qShift.Linecode", None),
                 FlightNum=fields.get("qShift.FlightNum", None),
-                SelectFlight=fields.get("qShift.BShift", None),
+                SelectFlight=fields.get("qShift.SelectFlight", None),
                 PortCode=fields.get("qShift.PortCode", None),
                 PortDestination=fields.get("qShift.PortDestination", None),
                 Shuttle=fields.get("qShift.Shuttle", None),
