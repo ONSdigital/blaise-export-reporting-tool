85a9a044f6809fe338be9a3b2337bc6bbdbaec07
Richmond Rice <richmond@ricey.org>
2025-12-10 11:05:45 +0000

    update test, bump libs

--- models/mi_hub_call_history_model.py
+++ models/mi_hub_call_history_model.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, fields
 from datetime import datetime, timedelta
 from typing import Optional, Union
 
+import pytz  # type: ignore
 
 from models.database_base_model import DatabaseBase
 
